07fec6ef8c81c61b298839fef2cd33ea5cb86055
FrumptiosCrown <anas200598@gmail.com>
2025-06-10 15:54:07 +0200

    Inpout code geändert

--- Assets/Scripts/InputSystem.cs
+++ Assets/Scripts/InputSystem.cs
@@ -11,7 +11,13 @@ public class InputSystem : MonoBehaviour
         playerMovement = new InputSystem_Actions();
         playerMovement.Player.Jump.performed += ctx => Debug.Log("Player jumped");
         playerMovement.Player.Move.performed += ctx => Debug.Log("Player moved");
-        playerMovement.Player.Look.performed += ctx => Debug.Log("Player looked");
+        playerMovement.Player.Jump.performed += ctx => Debug.Log(playerMovement.Player.Jump.triggered);
+        if (playerMovement.Player.Jump.triggered == true)
+        {
+            GameObject obj = new GameObject("Test Spawn Objekt!");
+            obj.AddComponent<Light>();
+            obj.transform.position = new Vector3(-2, 1, 0);
+        }
     }
 
     private void OnEnable()
